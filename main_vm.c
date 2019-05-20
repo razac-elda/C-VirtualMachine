db854941498f41d573a563b84d10f1791634ed07
Razac-elda <leo.mazzon97@gmail.com>
2019-05-21 00:26:32 +0200

    Prova di lettura file cvm

---
+++ main_vm.c
@@ -0,0 +1,37 @@
+/*
+Progetto virtual machine 2018-2019
+Membri gruppo: Leonardo Mazzon 868445, Giulio Nicola 875297
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(){
+
+	FILE *input;
+	int lung_riga;
+	size_t n = 0;
+	char *riga;
+	
+	/* Lettura test del file assembly.cvm */
+	input = fopen("assembly.cvm", "r");	
+	if(input){
+	
+		while((lung_riga = getline(&riga, &n, input)) != -1){
+			printf("%s\n", riga);
+			free(riga);
+			riga = NULL;
+		}
+		free(riga);
+		fclose(input);
+		
+	}else{
+		printf("Errore apertura file\n");
+	}
+	
+	
+	return 0;
+}
+
+
