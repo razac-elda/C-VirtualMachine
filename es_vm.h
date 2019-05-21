3d67b43d8fc4511a419cf22aedfa198a886fd014
giulionicola27 <giulionicola27@gmail.com>
2019-05-21 12:29:00 +0200

    file header

---
+++ es_vm.h
@@ -0,0 +1,6 @@
+/*
+Progetto virtual machine 2018-2019
+Membri gruppo: Leonardo Mazzon 868445, Giulio Nicola 875297
+*/
+#include "es_vm.h"
+
--- main_vm.c
+++ main_vm.c
@@ -2,7 +2,7 @@
 Progetto virtual machine 2018-2019
 Membri gruppo: Leonardo Mazzon 868445, Giulio Nicola 875297
 */
-
+#include "es_vm.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
